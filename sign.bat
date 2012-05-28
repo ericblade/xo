894759df16a584f0ab4ec44a295996c3788306cb
Eric Blade <blade.eric@gmail.com>
2012-05-28 18:02:24 -0400

    update bat file

--- sign.bat
+++ sign.bat
@@ -1,5 +1,5 @@
-del /y /q xobb.zip
-"c:\Program Files\7-Zip\7z.exe" u -xr!*.bat -xr!*.zip -xr!.git* xobb.zip *
+del /q xobb.zip
+"c:\Program Files\7-Zip\7z.exe" u -xr!*.bat -xr!*.zip -xr!.git* -xr!*.jar xobb.zip *
 set JAVA_HOME="c:\program files (x86)\Research In Motion\BlackBerry WebWorks SDK for TabletOS 2.2.0.5\jre"
 "c:\program files (x86)\Research In Motion\BlackBerry WebWorks SDK for TabletOS 2.2.0.5\bbwp\bbwp" c:\users\eric\ripplesites\xo\xobb.zip -o c:\temp\xobb -gcsk endgame -gp12 endgame
 "c:\program files (x86)\Research In Motion\BlackBerry WebWorks SDK for TabletOS 2.2.0.5\bbwp\blackberry-tablet-sdk\bin\blackberry-deploy" -installApp -password a -device 169.254.0.1 -package c:\temp\xobb\xobb.bar
